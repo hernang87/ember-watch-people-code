0f6255e93a38747863c362a171028ff15f8ac234
Hernan Garchtrom <hernang87@gmail.com>
2015-12-12 10:59:51 -0300

    Streamer page

--- app/routes/streamer.js
+++ app/routes/streamer.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model(params) {
-		this.store.findRecord('streamer', params.streamer).then((data) => {
-			console.log('OK');
+		return this.store.findRecord('streamer', params.streamer).then((data) => {
+			console.log('Streamer exists');
+			return data;
 		});
+	}, 
+	setupController(controller, model) {
+		controller.set('content', model);
 	}
 });
